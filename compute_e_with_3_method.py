4a6c461ce165b919b7496164dd45aca996e16fd0
aifred0729-TW <aifred0729tw@gmail.com>
2023-12-17 21:50:51 +0800

    first commit

---
+++ compute_e_with_3_method.py
@@ -0,0 +1,75 @@
+import time
+epsilon = 1e-15
+
+# Implementing the three methods to calculate the Euler number e in Python
+
+def midpoint(upper, lower, n):
+    summation = 0
+    hight = 0
+    d = (upper - lower) / n
+    for i in range(n):
+        hight = (1 / (lower + ((i*2+1)*d)/2))
+        summation += hight
+    summation *= d
+    return summation
+
+def compute_using_integral():
+    """
+    Calculates e using the integral definition with the midpoint integration method.
+    Stopping criterion: When the difference between consecutive estimates is less than epsilon.
+    """
+    start = time.process_time()
+    e = 1  # Initial guess for e
+    while True:
+        print(e)
+        tmp = abs(midpoint(e, 1, 100) - 1)
+        if tmp < 0.00001: break
+        e += 0.00001
+    end = time.process_time()
+
+    return e, (end-start)
+
+def compute_using_limit():
+    """
+    Calculates e using the limit definition.
+    Stopping criterion: When the increment in the value is less than epsilon.
+    """
+    start = time.process_time()
+    n = 1
+    e = (1 + 1 / n) ** n
+    while True:
+        n += 1
+        next_e = (1 + 1 / n) ** n
+        if abs(next_e - e) < epsilon:
+            break
+        e = next_e
+    end = time.process_time()
+    return e, (end-start)
+
+def compute_using_summation():
+    """
+    Calculates e using the series expansion.
+    Stopping criterion: When the term added is less than epsilon.
+    """
+    start = time.process_time()
+    e = 1
+    n = 1
+    factorial = 1
+    while True:
+        factorial *= n
+        term = 1 / factorial
+        if term < epsilon:
+            break
+        e += term
+        n += 1
+    end = time.process_time()
+    return e, (end-start)
+
+e_integral, time_integral = compute_using_integral()
+e_limit, time_limit = compute_using_limit()
+e_summation, time_summation = compute_using_summation()
+
+print("Compute using :")
+print("Integral  : %.15f - Time : %.10f"%(e_integral, time_integral))
+print("Limit     : %.15f - Time : %.10f"%(e_limit, time_limit))
+print("Summation : %.15f - Time : %.10f"%(e_summation, time_summation))
